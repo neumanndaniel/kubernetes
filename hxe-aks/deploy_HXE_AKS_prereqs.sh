d78748ae1e31542ef91648ac51ae3f019e2dc1e4
Daniel Neumann <mail@neumanndaniel.de>
2018-02-10 13:05:52 +0100

    Update deploy_HXE_AKS_prereqs.sh

--- hxe-aks/deploy_HXE_AKS_prereqs.sh
+++ hxe-aks/deploy_HXE_AKS_prereqs.sh
@@ -5,7 +5,7 @@ masterPassword=$1
 mkdir -p /data/hxe_pv
 chown 12000:79 /data/hxe_pv
 
-echo {"master_password" : "$masterPassword"} > /data/hxe_pv/password.json
+echo {'"'master_password'"' : '"'$masterPassword'"'} > /data/hxe_pv/password.json
 
 chmod 600 /data/hxe_pv/password.json
 chown 12000:79 /data/hxe_pv/password.json
