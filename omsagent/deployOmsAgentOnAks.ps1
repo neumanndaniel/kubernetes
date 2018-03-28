0c8d5550883e568da5255af5fd20dd5dfb531ba7
Daniel Neumann <mail@neumanndaniel.de>
2018-03-28 13:12:59 +0200

    Update deployOmsAgentOnAks.ps1

--- omsagent/deployOmsAgentOnAks.ps1
+++ omsagent/deployOmsAgentOnAks.ps1
@@ -1,5 +1,10 @@
+#Parameters for AKS OMS deployment
+Param(
+  [Parameter(Mandatory=$true,Position=1)]
+  [string]$omsWorkspaceName
+)
+
 #Variables for AKS OMS deployment
-$omsWorkspaceName='container-servicefabric'
 $gitHubTemplateUri='https://raw.githubusercontent.com/neumanndaniel/armtemplates/master/output/logAnalyticsWorkspace.json'
 $gitHubLogAnalyticsAgentUri='https://raw.githubusercontent.com/Microsoft/OMS-docker/master/Kubernetes/omsagent-ds-secrets.yaml'
 
@@ -9,23 +14,5 @@ $output=az group deployment create --resource-group operations-management --temp
 $workspaceId=$output.properties.outputs.workspaceId.value
 $primaryKey=$output.properties.outputs.primaryKey.value
 
-$workspaceIdEncoded=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($workspaceId))
-$primaryKeyEncoded=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($primaryKey))
-
-$yamlDefinition='apiVersion: v1
-data:
-  KEY: '+$primaryKeyEncoded+'
-  WSID: '+$workspaceIdEncoded+'
-kind: Secret
-metadata:
-  name: omsagent-secret
-  namespace: default
-type: Opaque'
-
-Write-Output $yamlDefinition > omsagent-secret.yaml
-
-kubectl apply -f ./omsagent-secret.yaml
-
-Invoke-WebRequest $gitHubLogAnalyticsAgentUri -OutFile ./oms-daemonset.yaml
-
-kubectl apply -f ./oms-daemonset.yaml
+$workspaceId
+$primayKey
