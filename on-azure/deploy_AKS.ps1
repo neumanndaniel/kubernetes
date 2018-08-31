6d078365959efd62a22f96acd6e8a12e8dce9a33
Daniel Neumann <neumanndanielde@outlook.com>
2018-08-31 12:19:18 +0200

    Update deploy_AKS.ps1

--- on-azure/deploy_AKS.ps1
+++ on-azure/deploy_AKS.ps1
@@ -5,8 +5,6 @@ $aksAciConnectorName='aciconnector'
 $acrRegistryName='aksdemoacr'
 $omsWorkspaceName='aks-demo-oms'
 $gitHubTemplateUri='https://raw.githubusercontent.com/neumanndaniel/armtemplates/master/operationsmanagement/aksMonitoringSolution.json'
-#$gitHubLogAnalyticsAgentUri='https://raw.githubusercontent.com/neumanndaniel/kubernetes/master/omsagent/oms-daemonset.yaml'
-#$dockerEmail='AKS@AzureRM'
 
 $inputKey=Read-Host '(1) West Europe
 (2) East US
@@ -25,6 +23,10 @@ switch ($inputKey.ToUpper()) {
     }
 }
 
+#Create Kubernetes service principal account
+Write-Output '>>Creating Kubernetes service principal account:'
+$kubernetesServicePrincipal = az ad sp create-for-rbac --skip-assignment --verbose
+
 #Create resource group
 Write-Output '>>Creating resource group:'
 az group create --name $resourceGroupName --location $azureRegion --output table
@@ -33,9 +35,14 @@ az group create --name $resourceGroupName --location $azureRegion --output table
 Write-Output '>>Creating ACR container registry:'
 az acr create --resource-group $resourceGroupName --name $acrRegistryName --sku Basic --admin-enabled true --output table
 
+#Assigning AKS Service Principal the reader role on ACR
+$acrId=$(az acr show --resource-group $resourceGroupName --name $acrRegistryName --query "id" --output tsv)
+
+az role assignment create --assignee ($kubernetesServicePrincipal|ConvertFrom-Json).appId --role Reader --scope $acrId --verbose --output table
+
 #Create AKS cluster
 Write-Output '>>Creating AKS cluster:'
-az aks create --resource-group $resourceGroupName --name $aksClusterName --node-count 1 --node-vm-size Standard_D2s_v3 --generate-ssh-keys --disable-rbac --output table
+az aks create --resource-group $resourceGroupName --name $aksClusterName --node-count 1 --node-vm-size Standard_D2s_v3 --generate-ssh-keys --disable-rbac --service-principal ($kubernetesServicePrincipal|ConvertFrom-Json).appId --client-secret ($kubernetesServicePrincipal|ConvertFrom-Json).password --output table
 
 #Getting AKS cluster credentials
 Write-Output '>>Getting AKS cluster credentials:'
@@ -46,36 +53,14 @@ Write-Output '>>Deploying ACI connector for Linux to AKS cluster:'
 helm init
 Write-Output '>>Waiting 30 seconds to spin up tiller pod:'
 Start-Sleep -Seconds 30
-az aks install-connector --resource-group $resourceGroupName --name $aksClusterName --connector-name $aksAciConnectorName
+az aks install-connector --resource-group $resourceGroupName --name $aksClusterName --connector-name $aksAciConnectorName --service-principal ($kubernetesServicePrincipal|ConvertFrom-Json).appId --client-secret ($kubernetesServicePrincipal|ConvertFrom-Json).password
 
-#Getting ACR container registry credentials and login
-#Write-Output '>>Getting ACR container registry credentials and create Kubernetes secret for ACR:'
-#$acrCredentials=az acr credential show --resource-group $resourceGroupName --name $acrRegistryName|ConvertFrom-Json
-
-#$acrUri=$acrRegistryName+'azurecr.io'
-#$dockerUsername=$acrCredentials.username
-#$dockerPassword=$acrCredentials.passwords[0].value
-
-#kubectl create secret docker-registry $acrRegistryName --docker-server=$acrUri --docker-email=$dockerEmail --docker-username=$dockerUsername --docker-password=$dockerPassword
-
-#Assigning Microsoft default AKS Service Principal (AzureContainerService) the reader role on ACR
-$clientId='7319c514-987d-4e9b-ac3d-d38c4f427f4c'
-$acrId=$(az acr show --resource-group $resourceGroupName --name $acrRegistryName --query "id" --output tsv)
-
-az role assignment create --assignee $clientId --role Reader --scope $acrId --verbose --output table
+$kubernetesServicePrincipal = $null
 
 #Create Log Analytics workspace, add the container monitoring solution to the workspace and deploy Log Analytics agent on the AKS cluster
 Write-Output '>>Creating Log Analytics workspace and deploy OMS agent to AKS cluster:'
 $null=az group deployment create --resource-group $resourceGroupName --template-uri $gitHubTemplateUri --parameters workspaceName=$omsWorkspaceName --verbose|ConvertFrom-Json
 
-#$workspaceId=$output.properties.outputs.workspaceId.value
-#$primaryKey=$output.properties.outputs.primaryKey.value
 $workspaceResourceId=az resource show --resource-group $resourceGroupName --name $omsWorkspaceName --resource-type 'Microsoft.OperationalInsights/workspaces' --verbose|ConvertFrom-Json
 
 az aks enable-addons --addons monitoring --resource-group $resourceGroupName --name $aksClusterName --workspace-resource-id $workspaceResourceId.id --output table
-
-#kubectl create secret generic omsagent-secret --from-literal=WSID=$workspaceId --from-literal=KEY=$primaryKey --namespace kube-system
-
-#Invoke-WebRequest $gitHubLogAnalyticsAgentUri -OutFile ./oms-daemonset.yaml
-
-#kubectl apply -f ./oms-daemonset.yaml
