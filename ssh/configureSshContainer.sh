d5aedc41b897583776a16a70cce5ab14f815df75
Daniel Neumann <neumanndanielde@outlook.com>
2019-05-14 23:45:20 +0200

    Create configureSshContainer.sh

---
+++ ssh/configureSshContainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl exec aks-ssh -c aks-ssh -- apk update
+kubectl exec aks-ssh -c aks-ssh -- apk add openssh-client bash
+kubectl cp ~/.ssh/id_rsa aks-ssh:/id_rsa
+kubectl exec aks-ssh -c aks-ssh chmod 0600 id_rsa
