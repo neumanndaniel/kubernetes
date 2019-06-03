08c26c4c2f21ddaec6f82a643326e9b56dee4fc6
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2019-06-04 00:05:48 +0200

    Create install-k3s-master.sh

---
+++ k3s/install-k3s-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MASTER=$(hostname)
+
+curl -sfL https://get.k3s.io -o install.sh
+chmod +x install.sh
+./install.sh server --kubelet-arg="address=0.0.0.0"
+systemctl status k3s
+
+sudo apt update
+sudo apt install jq vim git -y
+kubectl taint nodes $MASTER node-role.kubernetes.io/master=true:NoSchedule
+kubectl label node $MASTER kubernetes.io/role=master node-role.kubernetes.io/master=
