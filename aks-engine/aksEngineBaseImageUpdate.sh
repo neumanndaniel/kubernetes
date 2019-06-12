93b443a59e0b2cf4bfbcbe1829fd40fbaba974d1
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2019-06-12 23:16:19 +0200

    Update aksEngineBaseImageUpdate.sh

--- aks-engine/aksEngineBaseImageUpdate.sh
+++ aks-engine/aksEngineBaseImageUpdate.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 MSIENABLED=$(sudo cat /etc/kubernetes/azure.json | grep aadClientId | cut -d '"' -f4)
 if [ "$MSIENABLED" = "msi" ]
-then
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster uses MSI and is therefore supported by the script. Script continues..."
-else
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster does not use MSI and is not supported by the script. Exiting the script..."
-    exit
+    then
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster uses MSI and is therefore supported by the script. Script continues..."
+    else
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster does not use MSI and is not supported by the script. Exiting the script..."
+        exit
 fi
 
 AZCLI=$(which az)
 if [ -z "$AZCLI" ]
-then
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] No Azure CLI installed. Installing Azure CLI..."
-    sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
-    AZ_REPO=$(lsb_release -cs)
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-        sudo tee /etc/apt/sources.list.d/azure-cli.list
-    sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-        --keyserver packages.microsoft.com \
-        --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
-    sudo apt-get update
-    sudo apt-get install azure-cli
-else
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Azure CLI installed. Script continues with updating the base image..."
+    then
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] No Azure CLI installed. Installing Azure CLI..."
+        sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+        AZ_REPO=$(lsb_release -cs)
+        echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+            sudo tee /etc/apt/sources.list.d/azure-cli.list
+        sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+            --keyserver packages.microsoft.com \
+            --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+        sudo apt-get update
+        sudo apt-get install azure-cli
+    else
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Azure CLI installed. Script continues with updating the base image..."
 fi
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Logging in to Azure via Managed Service Identity..."
@@ -41,9 +44,9 @@ SKU=$SKUTEMP$(date +"%Y%m")
 
 BASEIMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
 if [ $(echo $BASEIMAGES | jq length) -eq 0 ]
-then
-    SKU=$SKUTEMP$(date +"%Y%m" --date="last month")
-    BASEIMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
+    then
+        SKU=$SKUTEMP$(date +"%Y%m" --date="last month")
+        BASEIMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
 fi
 BASEIMAGESCOUNT=$(echo $BASEIMAGES|jq length)
 LATESTBASEIMAGE=$(echo $BASEIMAGES| jq .[$BASEIMAGECOUNT-1])
@@ -51,21 +54,21 @@ LATESTBASEIMAGE=$(echo $BASEIMAGES| jq .[$BASEIMAGECOUNT-1])
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating base image..."
 az vmss update --resource-group $RESOURCEGROUP --name $VMSS \
     --set virtualMachineProfile.storageProfile.imageReference.sku=$(echo $LATESTBASEIMAGE|jq .sku|cut -d '"' -f 2) \
-        virtualMachineProfile.storageProfile.imageReference.version=$(echo $LATESTBASEIMAGE|jq .version|cut -d '"' -f 2)| jq .virtualMachineProfile.storageProfile.imageReference
+    virtualMachineProfile.storageProfile.imageReference.version=$(echo $LATESTBASEIMAGE|jq .version|cut -d '"' -f 2)| jq .virtualMachineProfile.storageProfile.imageReference
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instances..."
 VMSSINSTANCES=$(kubectl get nodes|grep vmss |cut -d ' ' -f1)
 
 for ITEM in $VMSSINSTANCES
-do
-    TEMPINSTANCEID=$(kubectl get nodes $ITEM -o yaml|grep providerID)
-    INSTANCEID=$(echo $TEMPINSTANCEID|cut -d '/' -f13)
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Draining node $ITEM..."
-    kubectl drain $ITEM --ignore-daemonsets --delete-local-data --force
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instance $ITEM..."
-    az vmss update-instances --instance-ids $INSTANCEID --name $VMSS --resource-group $RESOURCEGROUP
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Uncordon node $ITEM..."
-    kubectl uncordon $ITEM
-done
+    do
+        TEMPINSTANCEID=$(kubectl get nodes $ITEM -o yaml|grep providerID)
+        INSTANCEID=$(echo $TEMPINSTANCEID|cut -d '/' -f13)
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Draining node $ITEM..."
+        kubectl drain $ITEM --ignore-daemonsets --delete-local-data --force
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instance $ITEM..."
+        az vmss update-instances --instance-ids $INSTANCEID --name $VMSS --resource-group $RESOURCEGROUP
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Uncordon node $ITEM..."
+        kubectl uncordon $ITEM
+    done
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Base image update finished..."
