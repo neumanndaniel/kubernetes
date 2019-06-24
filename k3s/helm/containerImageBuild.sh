aacdb5b67e20cbe36b5aa1b46baf1894465a259f
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2019-06-24 23:55:42 +0200

    Update containerImageBuild.sh

--- k3s/helm/containerImageBuild.sh
+++ k3s/helm/containerImageBuild.sh
@@ -6,4 +6,4 @@ BUILDNUMBER=$1
 
 VERSION=$(cat HELMVERSION)
 
-az acr build --registry azstcr --image "tiller:$VERSION" --image "tiller:$BUILDNUMBER" .
+az acr build --registry acr --image "tiller:$VERSION" --image "tiller:$BUILDNUMBER" .
