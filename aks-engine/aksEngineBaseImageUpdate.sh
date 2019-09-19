e6cb4ec40217b3705066580c9ead949630c5df66
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2019-09-19 22:49:17 +0200

    Add multiple node pools support

--- aks-engine/aksEngineBaseImageUpdate.sh
+++ aks-engine/aksEngineBaseImageUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MSI_ENABLED=$(sudo cat /etc/kubernetes/azure.json|grep aadClientId|cut -d '"' -f4)
-if [[ "$MSI_ENABLED" = "msi" ]]; then
+if [[ "$MSI_ENABLED" == "msi" ]]; then
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster uses MSI and is therefore supported by the script. Script continues..."
 else
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] AKS Engine cluster does not use MSI and is not supported by the script. Exiting the script..."
@@ -30,41 +30,48 @@ set -o pipefail
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Logging in to Azure via Managed Service Identity..."
 NULL=$(az login --identity)
 
-echo "[$(date +"%Y-%m-%d %H:%M:%S")] Gathering information about the Kubernetes cluster and the latest base image..."
-VMSS=$(kubectl get nodes|grep vmss --max-count=1|cut -d ' ' -f1|rev|cut -c 7-|rev)
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Gathering information about the AKS Engine cluster and the latest base image..."
 RESOURCE_GROUP=$(kubectl get node $(kubectl get nodes|grep vmss --max-count=1|cut -d ' ' -f 1) -o json|jq '.metadata.labels'|grep kubernetes.azure.com/cluster|cut -d '"' -f4)
+VM_SCALE_SETS=$(az vmss list --resource-group $RESOURCE_GROUP| jq -r '.[].name')
 
-VMSS_PROPERTIES=$(az vmss show --resource-group $RESOURCE_GROUP --name $VMSS)
-OFFER=$(echo $VMSS_PROPERTIES| jq '.virtualMachineProfile.storageProfile.imageReference.offer'|cut -d '"' -f 2)
-PUBLISHER=$(echo $VMSS_PROPERTIES| jq '.virtualMachineProfile.storageProfile.imageReference.publisher'|cut -d '"' -f 2)
-SKU_TEMP=$(echo $VMSS_PROPERTIES| jq '.virtualMachineProfile.storageProfile.imageReference.sku'|cut -d '"' -f 2|rev|cut -c 7-|rev)
-SKU=$SKU_TEMP$(date +"%Y%m")
+for VMSS in $VM_SCALE_SETS; do
+    VMSS_PROPERTIES=$(az vmss show --resource-group $RESOURCE_GROUP --name $VMSS)
 
-BASE_IMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
-if [[ $(echo $BASE_IMAGES|jq length) -eq 0 ]]; then
-    SKU=$SKU_TEMP$(date +"%Y%m" --date="last month")
+    OFFER=$(echo $VMSS_PROPERTIES|jq -r '.virtualMachineProfile.storageProfile.imageReference.offer')
+    PUBLISHER=$(echo $VMSS_PROPERTIES|jq -r '.virtualMachineProfile.storageProfile.imageReference.publisher')
+    SKU_TEMP=$(echo $VMSS_PROPERTIES|jq -r '.virtualMachineProfile.storageProfile.imageReference.sku'|rev|cut -c 7-|rev)
+    SKU=$SKU_TEMP$(date +"%Y%m")
+
     BASE_IMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
-fi
-BASE_IMAGE_COUNT=$(echo $BASE_IMAGES|jq length)
-LATESTBASEIMAGE=$(echo $BASE_IMAGES|jq ".[$BASE_IMAGE_COUNT-1]")
+    if [[ $(echo $BASE_IMAGES|jq length) -eq 0 ]]; then
+        SKU=$SKU_TEMP$(date +"%Y%m" --date="last month")
+        BASE_IMAGES=$(az vm image list --offer $OFFER --publisher $PUBLISHER --sku $SKU --all)
+    fi
+    BASE_IMAGE_COUNT=$(echo $BASE_IMAGES|jq length)
+    LATEST_BASE_IMAGE=$(echo $BASE_IMAGES|jq ".[$BASE_IMAGE_COUNT-1]")
 
-echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating base image..."
-az vmss update --resource-group $RESOURCE_GROUP --name $VMSS \
-    --set virtualMachineProfile.storageProfile.imageReference.sku=$(echo $LATESTBASEIMAGE|jq '.sku'|cut -d '"' -f 2) \
-    virtualMachineProfile.storageProfile.imageReference.version=$(echo $LATESTBASEIMAGE|jq '.version'|cut -d '"' -f 2)|jq '.virtualMachineProfile.storageProfile.imageReference'
+    if [[ $(echo $LATEST_BASE_IMAGE|jq -r '.version') != $(echo $VMSS_PROPERTIES|jq -r '.virtualMachineProfile.storageProfile.imageReference.version') ]]; then
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating base image for VMSS $VMSS..."
+        az vmss update --resource-group $RESOURCE_GROUP --name $VMSS \
+            --set virtualMachineProfile.storageProfile.imageReference.sku=$(echo $LATEST_BASE_IMAGE|jq -r '.sku') \
+                virtualMachineProfile.storageProfile.imageReference.version=$(echo $LATEST_BASE_IMAGE|jq -r '.version')|jq '.virtualMachineProfile.storageProfile.imageReference'
 
-echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instances..."
-VMSS_INSTANCES=$(kubectl get nodes|grep vmss|cut -d ' ' -f1)
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instances..."
+        VMSS_INSTANCES=$(kubectl get nodes|grep $VMSS|cut -d ' ' -f1)
 
-for ITEM in $VMSS_INSTANCES; do
-    TEMP_INSTANCE_ID=$(kubectl get nodes $ITEM -o yaml|grep providerID)
-    INSTANCE_ID=$(echo $TEMP_INSTANCE_ID|cut -d '/' -f13)
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Draining node $ITEM..."
-    kubectl drain $ITEM --ignore-daemonsets --delete-local-data --force
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instance $ITEM..."
-    az vmss update-instances --instance-ids $INSTANCE_ID --name $VMSS --resource-group $RESOURCE_GROUP
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Uncordon node $ITEM..."
-    kubectl uncordon $ITEM
+        for INSTANCE in $VMSS_INSTANCES; do
+            TEMP_INSTANCE_ID=$(kubectl get nodes $INSTANCE -o yaml|grep providerID)
+            INSTANCE_ID=$(echo $TEMP_INSTANCE_ID|cut -d '/' -f13)
+            echo "[$(date +"%Y-%m-%d %H:%M:%S")] Draining node $INSTANCE..."
+            kubectl drain $INSTANCE --ignore-daemonsets --delete-local-data --force
+            echo "[$(date +"%Y-%m-%d %H:%M:%S")] Updating VMSS instance $INSTANCE..."
+            az vmss update-instances --instance-ids $INSTANCE_ID --name $VMSS --resource-group $RESOURCE_GROUP
+            echo "[$(date +"%Y-%m-%d %H:%M:%S")] Uncordon node $INSTANCE..."
+            kubectl uncordon $INSTANCE
+        done
+    else
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Skipping VMSS $VMSS. No newer base image version available..."
+    fi
 done
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Base image update finished..."
