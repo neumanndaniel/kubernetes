71c66ae23be92775d9f8d431e678b5af8109b488
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2021-09-03 14:00:13 +0200

    Add namespace switch to kube-system

--- kind/setup.sh
+++ kind/setup.sh
@@ -8,7 +8,11 @@ kind create cluster --config=single-node.yaml
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl apply -f ./calico-config.yaml
 
+sleep 120
+
 # Metrics Server
+kubectl config set-context --current --namespace kube-system
+
 helm upgrade metrics-server --install \
 --set apiService.create=true \
 --set extraArgs.kubelet-insecure-tls=true \
