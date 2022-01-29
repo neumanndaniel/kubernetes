0db2a508054f5d3958044e77fcc1f00a58fe1a97
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2022-01-29 23:51:51 +0100

    Update files

--- gvisor/run.sh
+++ gvisor/run.sh
@@ -2,17 +2,15 @@
 
 URL="https://raw.githubusercontent.com/neumanndaniel/kubernetes/master/gvisor/config.toml"
 
-wget ${URL}
+wget ${URL} -O /k8s-node/config.toml
 cp /install-gvisor.sh /k8s-node
-cp /config.toml /k8s-node
 
 /usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/gvisor/install-gvisor.sh
 /usr/bin/nsenter -m/proc/1/ns/mnt /tmp/gvisor/install-gvisor.sh
-/usr/bin/nsenter -m/proc/1/ns/mnt -- cp /etc/containerd/config.toml /tmp/gvisor/config.toml.org
+/usr/bin/nsenter -m/proc/1/ns/mnt -- cp /etc/containerd/config.toml /etc/containerd/config.toml.org
 /usr/bin/nsenter -m/proc/1/ns/mnt -- cp /tmp/gvisor/config.toml /etc/containerd/config.toml
 /usr/bin/nsenter -m/proc/1/ns/mnt -- systemctl restart containerd
 
-NODE_NAME=$(/usr/bin/nsenter -m/proc/1/ns/mnt -- cat /etc/hostname)
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Successfully installed gvisor and restarted containerd on node ${NODE_NAME}."
 
 sleep infinity
